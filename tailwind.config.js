4dd07a5c2e549dbdb97411fb4c5dee2f05f56d22
Brennan Duck <brennantduck@gmail.com>
2024-09-03 14:35:08 -0400

    sdfafds

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from "daisyui";
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,svelte}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [daisyui],
-    daisyui: {
-        themes: ["cmyk"],
-    },
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,svelte}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        earth: {
+          primary: "#c2073f",
+          secondary: "#470216",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#058f65",
+        },
+      },
+    ],
+  },
 };
